67246991ab70c6d3547e521b6ab1f4d77437acfa
jubair <hmjubair12@gmail.com>
2026-01-01 01:10:23 +0600

    chore: add default values for variables in terraform.tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "MyResourceGroup"
+subnet-name          = "subnet-2"
+azureVnet_name      = "my-vnet-1"
--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "resource_group_name" {
   type    = string
-  default = "MyResourceGroup"
 }
 
 variable "subnet-name" {
   type    = string
-  default = "subnet-2"
 }
 
 variable "azureVnet_name" {
   type = string
-  default = "my-vnet-1"
 }
